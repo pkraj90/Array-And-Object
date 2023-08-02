b4106eeb7f2f7e4399c9fd0c4c6d2cb02d08f73e
pkraj90 <pkraj06@gmail.com>
2023-08-03 01:44:44 +0530

    content updated

--- Script.js
+++ Script.js
@@ -32,7 +32,43 @@ A.place ="Madurai" // add place
 delete A.Age// Remove object property.
 console.log(A);
 
+// JSON - Array & Object
+//JSON Array //const c = ["apple", "banana", "orange"] 
+//JSON Object 
+const d = '{"name":"Raj","age":"32","place":"madurai"}';
+console.log(d);
+//JSON.parse() - syntax
+var e = JSON.parse(d);
+console.log(e);
+//JSON.stringify()
+console.log(A);
+console.log(JSON.stringify(A));
 
+//Nested JSON object
+const f = {
+   name: "Rajkumar",
+    age: 32,
+    address: {
+        place: "Sivagangai",
+        pincode: 630611
+    }
+}
+console.log(f)
+console.log(f.address.place)
 
-
-
+//Nested JSON Array
+const g = [
+    {
+        name:"Raj",
+        age:30
+    },
+    {
+        name:"Prakash",
+        age:35
+    }
+]
+console.log(g);
+console.log(g[0]);
+console.log(g[0].name);
+console.log(g[1]);
+console.log(g[1].age);
