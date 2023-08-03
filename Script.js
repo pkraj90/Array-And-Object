0eaed4699e9c26231c602acfb89aa30a75adcd7d
pkraj90 <pkraj06@gmail.com>
2023-08-03 09:08:13 +0530

    content updated

--- Script.js
+++ Script.js
@@ -72,3 +72,60 @@ console.log(g[0]);
 console.log(g[0].name);
 console.log(g[1]);
 console.log(g[1].age);
+
+// Array & Object destructuring
+const arr = [1,2,3];
+const [x,y,z] = arr;
+console.log(x);
+console.log(y);
+console.log(z);
+
+//object destructuring
+const obj = {
+    name1: "RK",
+    age: 30,
+    address: {
+        place: "Keeladi",
+        pin: 625201
+    }
+   
+}
+const {name1,age,address:{place,pin}} = obj;
+console.log(name1);
+console.log(age);
+console.log(place);
+console.log(pin);
+
+const {Name:Myname,work,year=90} = A;
+console.log(Myname);
+console.log(work);
+console.log(year);
+//age deleted in A by modify
+
+//"this"-keyword method
+//1."this" alone - referes to global object
+console.log(this);
+
+//2.In regular function , refers to global object
+const az = function(){
+    console.log("Hi Raj");
+    console.log(this)
+}
+az();
+//3. In a function in strict mode , "this" is undefined.
+"use strict";
+const bz = function(){
+    console.log("strict mode");
+    console.log(this)
+}
+bz();
+//4. In a method, "this" refers to the owner object.
+const person = {
+    names : "Raja",
+    age: 30,
+    occupation: "Police",
+    detail: function(){
+        console.log(`I love ❤ ${this.occupation}`)
+    }
+}
+console.log(person.detail());
